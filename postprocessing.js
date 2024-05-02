4ddef5f6f4f40f9e7ad80f471734c2628da17d61
emikee <eks2063@nyu.edu>
2024-05-02 12:52:29 -0400

    bokehpass

--- postprocessing.js
+++ postprocessing.js
@@ -2,14 +2,10 @@ import {UnrealBloomPass} from 'three/examples/jsm/postprocessing/UnrealBloomPass
 import {EffectComposer} from 'three/examples/jsm/postprocessing/EffectComposer'
 import {OutputPass} from 'three/examples/jsm/postprocessing/OutputPass'
 import {RenderPass} from 'three/examples/jsm/postprocessing/RenderPass'
-
-//doesn't work :(
-//import {ShaderPass} from 'three/examples/jsm/postprocessing/ShaderPass.js';
-//import {DepthLimitedBlurShader} from 'three/examples/jsm/shaders/DepthLimitedBlurShader.js'
-
+import {BokehPass} from 'three/examples/jsm/postprocessing/BokehPass'
 
 export function postprocessing(scene, camera, renderer) {
-    
+
     //set up
     const composer = new EffectComposer(renderer)
     composer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
@@ -18,9 +14,12 @@ export function postprocessing(scene, camera, renderer) {
     const renderPass = new RenderPass(scene, camera)
     composer.addPass(renderPass)
 
-    //doF attempt
-    // const dofPass = new ShaderPass(DepthLimitedBlurShader)
-    // composer.addPass(dofPass)
+    // const bokehPass = new BokehPass(scene, camera, {
+    //     focus: 100,
+    //     aperature: 5,
+    //     maxblur: 0.01
+    // })
+    // composer.addPass(bokehPass)
 
     const bloomPass = new UnrealBloomPass()
     bloomPass.strength = 0.2
